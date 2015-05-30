568cc995eb0321622394371a355c5c61c46405bd
Nikita Gusakov <dev@nkt.me>
2015-05-30 10:22:22 +0300

    Added tests

---
+++ test/remove-react-prop-types.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const fs = require('fs');
+const assert = require('assert');
+const babel = require('babel-core');
+const reactPlugin = require('../src/index');
+
+function trim(str) {
+  return str.replace(/^\s+|\s+$/, '');
+}
+
+describe('remove react propTypes', () => {
+  const fixturesDir = path.join(__dirname, 'fixtures');
+  fs.readdirSync(fixturesDir).map((caseName) => {
+    it(`should ${caseName.split('-').join(' ')}`, () => {
+      const fixtureDir = path.join(fixturesDir, caseName);
+      const actual = babel.transformFileSync(path.join(fixtureDir, 'actual.js'), {
+        plugins: [
+          reactPlugin
+        ]
+      }).code;
+      const expected = fs.readFileSync(path.join(fixtureDir, 'expected.js')).toString();
+
+      assert.equal(trim(actual), trim(expected));
+    });
+  });
+});
