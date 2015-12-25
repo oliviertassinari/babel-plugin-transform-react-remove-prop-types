b9aabe4eecea7b269ecfe6d56e076c924dc50384
Olivier Tassinari <olivier.tassinari@gmail.com>
2015-12-25 22:59:23 +0100

    [babel] Upgrade to v6

--- src/index.js
+++ src/index.js
@@ -1,56 +1,68 @@
 export default function ({ Plugin, types: t }) {
-  const visitor = {
-    Property: {
-      exit(node) {
-        if (node.computed || node.key.name !== 'propTypes') {
-          return;
-        }
+  return {
+    visitor: {
+      Program(path) {
+        // On program start, do an explicit traversal up front for this plugin.
+        path.traverse({
+          ObjectProperty: {
+            exit(path) {
+              const node = path.node;
 
-        const parent = this.findParent((parent) => {
-          if (parent.type !== 'CallExpression') {
-            return false;
-          }
+              if (node.computed || node.key.name !== 'propTypes') {
+                return;
+              }
 
-          return parent.get('callee').matchesPattern('React.createClass');
+              const parent = path.findParent((parent) => {
+                if (parent.type !== 'CallExpression') {
+                  return false;
+                }
+
+                return parent.get('callee').matchesPattern('React.createClass');
+              });
+
+              if (parent) {
+                path.remove();
+              }
+            }
+          },
+          ClassProperty(path) {
+            const {
+              node,
+              scope,
+            } = path;
+
+            if (node.key.name === 'propTypes') {
+              const className = scope.block.id.name;
+              const binding = scope.getBinding(className);
+              const superClass = binding.path.get('superClass');
+              if (superClass.matchesPattern('React.Component') || superClass.matchesPattern('Component')) {
+                path.remove();
+              }
+            }
+          },
+          AssignmentExpression(path) {
+            const {
+              node,
+              scope,
+            } = path;
+
+            if (node.left.computed || !node.left.property || node.left.property.name !== 'propTypes') {
+              return;
+            }
+
+            const className = node.left.object.name;
+            const binding = scope.getBinding(className);
+            if (!binding || !binding.path.isClassDeclaration()) {
+              return;
+            }
+
+            const superClass = binding.path.get('superClass');
+            if (superClass.matchesPattern('React.Component') || superClass.matchesPattern('Component')) {
+              path.remove();
+            }
+          },
         });
-
-        if (parent) {
-          this.dangerouslyRemove();
-        }
-      }
+      },
     },
-    ClassProperty(node, parent, scope) {
-      if (node.key.name === 'propTypes') {
-        const className = scope.block.id.name;
-        const binding = this.scope.getBinding(className);
-        const superClass = binding.path.get('superClass');
-        if (superClass.matchesPattern('React.Component') || superClass.matchesPattern('Component')) {
-          this.dangerouslyRemove();
-        }
-      }
-    },
-    AssignmentExpression(node) {
-      if (node.left.computed || !node.left.property || node.left.property.name !== 'propTypes') {
-        return;
-      }
-
-      const className = node.left.object.name;
-      const binding = this.scope.getBinding(className);
-      if (!binding || !binding.path.isClassDeclaration()) {
-        return;
-      }
-
-      const superClass = binding.path.get('superClass');
-      if (superClass.matchesPattern('React.Component') || superClass.matchesPattern('Component')) {
-        this.dangerouslyRemove();
-      }
-    }
   };
-
-  return new Plugin('react-remove-prop-types', {
-    visitor,
-    metadata: {
-      group: 'builtin-pre'
-    }
-  });
 };
--- test/index.js
+++ test/index.js
@@ -1,8 +1,8 @@
-const path = require('path');
-const fs = require('fs');
-const assert = require('assert');
-const babel = require('babel-core');
-const reactPlugin = require('../src/index');
+import path from 'path';
+import fs from 'fs';
+import assert from 'assert';
+import {transformFileSync} from 'babel-core';
+import reactPlugin from '../src/index';
 
 function trim(str) {
   return str.replace(/^\s+|\s+$/, '');
@@ -13,8 +13,7 @@ describe('remove react propTypes', () => {
   fs.readdirSync(fixturesDir).map((caseName) => {
     it(`should ${caseName.split('-').join(' ')}`, () => {
       const fixtureDir = path.join(fixturesDir, caseName);
-      const actual = babel.transformFileSync(path.join(fixtureDir, 'actual.js'), {
-        stage: 0,
+      const actual = transformFileSync(path.join(fixtureDir, 'actual.js'), {
         plugins: [
           reactPlugin
         ]
