78f869daf7893fe44e3380b0556fce0c163d4671
Olivier Tassinari <olivier.tassinari@gmail.com>
2016-08-01 23:25:11 +0200

    [Class] Fix an issue with the export syntax

--- test/fixtures/es-class-static-property/expected-wrap.js
+++ test/fixtures/es-class-static-property/expected-wrap.js
@@ -1,5 +1,9 @@
 "use strict";
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -8,23 +12,45 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var Foo = function (_React$Component) {
-  _inherits(Foo, _React$Component);
+var Foo1 = function (_React$Component) {
+  _inherits(Foo1, _React$Component);
 
-  function Foo() {
-    _classCallCheck(this, Foo);
+  function Foo1() {
+    _classCallCheck(this, Foo1);
 
-    return _possibleConstructorReturn(this, Object.getPrototypeOf(Foo).apply(this, arguments));
+    return _possibleConstructorReturn(this, Object.getPrototypeOf(Foo1).apply(this, arguments));
   }
 
-  _createClass(Foo, [{
+  _createClass(Foo1, [{
     key: "render",
     value: function render() {}
   }]);
 
-  return Foo;
+  return Foo1;
 }(React.Component);
 
-process.env.NODE_ENV !== "production" ? Foo.propTypes = {
-  foo: React.PropTypes.string
+process.env.NODE_ENV !== "production" ? Foo1.propTypes = {
+  bar1: React.PropTypes.string
+} : void 0;
+
+var Foo2 = function (_React$Component2) {
+  _inherits(Foo2, _React$Component2);
+
+  function Foo2() {
+    _classCallCheck(this, Foo2);
+
+    return _possibleConstructorReturn(this, Object.getPrototypeOf(Foo2).apply(this, arguments));
+  }
+
+  _createClass(Foo2, [{
+    key: "render",
+    value: function render() {}
+  }]);
+
+  return Foo2;
+}(React.Component);
+
+exports.default = Foo2;
+process.env.NODE_ENV !== "production" ? Foo2.propTypes = {
+  bar2: React.PropTypes.string
 } : void 0;
