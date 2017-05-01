463d987fcc2cbf322440162a7a67ac3c14dce6ad
Olivier Tassinari <olivier.tassinari@gmail.com>
2017-05-01 19:30:53 +0200

    fix(flow): use case

---
+++ test/fixtures/flow/actual.js
@@ -0,0 +1,23 @@
+class Foo1 extends Component {
+  props:  {
+    bar1?: string,
+  };
+  render() {}
+}
+
+type Props2 = {
+  bar2?: string
+}
+
+class Foo2 extends Component {
+  props: Props2;
+  render() {}
+}
+
+type Props3 = {
+  bar3?: string,
+}
+
+function Foo3(props: Props3) {
+  return <div />;
+}
