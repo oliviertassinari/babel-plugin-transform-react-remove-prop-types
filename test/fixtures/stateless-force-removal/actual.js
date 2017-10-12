15901b32e19ef500bd7e2fbbdcb0ca147ff95fbf
Benjamin Bouvier <public@benj.me>
2017-10-12 20:43:09 +0200

    Force removal should abort early;

---
+++ test/fixtures/stateless-force-removal/actual.js
@@ -0,0 +1,31 @@
+import React, { PropTypes } from 'react';
+
+var Message = ({isFetching, isSuccess, isFailure, errorMsg}) => {
+  let messageType;
+  let messageTxt;
+  if (isFetching) {
+    messageType = 'warning';
+    messageTxt = 'Pending call...';
+  } else if (isSuccess) {
+    messageType = 'success';
+    messageTxt = 'Repo pushed successfully';
+  } else if (isFailure) {
+    messageType = 'danger';
+    messageTxt = 'Something wrong occured';
+  }
+
+  if (messageTxt === null) {
+    return;
+  }
+
+  return <div className={'alert alert-' + messageType} role='alert'>{messageTxt}</div>;
+};
+
+Message.propTypes /* remove-proptypes */ = {
+  isFetching: PropTypes.bool.isRequired,
+  isSuccess: PropTypes.bool.isRequired,
+  isFailure: PropTypes.bool.isRequired,
+  errorMsg: PropTypes.string.isRequired
+};
+
+export default Message;
