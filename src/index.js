56a7de5fd193434d31e66a52ef4213700fc0f6bf
Joe Lencioni <joe.lencioni@airbnb.com>
2018-08-16 13:43:00 -0700

    Add early bailout in areSetsEqual()
    Although this probably doesn't matter for our use-case, it is more
    complete.

--- src/index.js
+++ src/index.js
@@ -54,6 +54,10 @@ function isReactClass(superClass, scope, globalOptions) {
 }
 
 function areSetsEqual(set1, set2) {
+  if (set1 === set2) {
+    return true
+  }
+
   if (set1.size !== set2.size) {
     return false
   }
