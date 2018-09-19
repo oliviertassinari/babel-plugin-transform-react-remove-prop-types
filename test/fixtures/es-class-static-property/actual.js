5be830f5eb765ac3599f265fa4b56180df13f694
Joe Lencioni <joe.lencioni@airbnb.com>
2018-09-19 13:52:25 -0700

    Add failing test case for #153
    I have been investigating a bug that occurs when a ClassExpression is
    using the experimental class properties for propTypes. I am not sure
    what the best fix for it is yet, but I wanted to get up a failing test
    case to help showcase the bug.

--- test/fixtures/es-class-static-property/actual.js
+++ test/fixtures/es-class-static-property/actual.js
@@ -13,3 +13,13 @@ export default class Foo2 extends React.Component {
 
   render() {}
 }
+
+const componentFactory = () => {
+  return class Foo3 extends React.Component {
+    static propTypes = {
+      bar3: PropTypes.string,
+    }
+
+    render() {}
+  };
+};
--- test/fixtures/es-class-static-property/expected-wrap-es6.js
+++ test/fixtures/es-class-static-property/expected-wrap-es6.js
@@ -13,3 +13,14 @@ export default class Foo2 extends React.Component {
 Foo2.propTypes = process.env.NODE_ENV !== "production" ? {
   bar2: PropTypes.string
 } : {};
+
+const componentFactory = () => {
+  var _temp;
+
+  return _temp = class Foo3 extends React.Component {
+    render() {}
+
+  }, Foo3.propTypes = process.env.NODE_ENV !== "production" ? {
+    bar3: PropTypes.string
+  } : {}, _temp;
+};
