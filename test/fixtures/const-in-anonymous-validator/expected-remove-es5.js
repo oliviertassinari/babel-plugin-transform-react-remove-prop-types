d89cadb2ff727d43a51b629b6561d601e3e8b566
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2018-10-15 09:39:44 +0200

    Add failing test case for const scoped

---
+++ test/fixtures/const-in-anonymous-validator/expected-remove-es5.js
@@ -0,0 +1,15 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = babelHelpers.interopRequireWildcard(require("react"));
+
+var Component = function Component() {
+  return _react.default.createElement("div", null);
+};
+
+var _default = Component;
+exports.default = _default;
---
+++ test/fixtures/const-in-anonymous-validator/expected-wrap-es5.js
@@ -0,0 +1,21 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = babelHelpers.interopRequireWildcard(require("react"));
+
+var Component = function Component() {
+  return _react.default.createElement("div", null);
+};
+
+Component.propTypes = process.env.NODE_ENV !== "production" ? {
+  variant: function variant(props) {
+    var variants = process.env.NODE_ENV !== "production" ? [null] : {};
+    return variantſ[0];
+  }
+} : {};
+var _default = Component;
+exports.default = _default;
