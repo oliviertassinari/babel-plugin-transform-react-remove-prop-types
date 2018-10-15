9a3fc497fa96728e4d8c4286f7564d3fbedd7c85
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2018-10-15 09:39:44 +0200

    Add failing test case for const scoped

---
+++ test/fixtures/const-in-anonymous-validator/actual.js
@@ -0,0 +1,14 @@
+import React, {PropTypes} from 'react';
+
+const Component = () => (
+  <div />
+);
+
+Component.propTypes = {
+  variant: props => {
+    const variants = [null];
+    return variantſ[0];
+  }
+};
+
+export default Component;
---
+++ test/fixtures/const-in-anonymous-validator/expected-wrap-es6.js
@@ -0,0 +1,11 @@
+import React, { PropTypes } from 'react';
+
+const Component = () => <div />;
+
+Component.propTypes = process.env.NODE_ENV !== "production" ? {
+  variant: props => {
+    const variants = process.env.NODE_ENV !== "production" ? [null] : {};
+    return variantſ[0];
+  }
+} : {};
+export default Component;
