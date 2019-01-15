50f9bca8db632f642a631f269321884687aa5b0b
Tony Quetano <tquetano@netflix.com>
2019-01-15 10:40:54 -0800

    make test more realistic

--- test/fixtures/react-create-class-custom-name-no-property-name/expected-remove-es6.js
+++ test/fixtures/react-create-class-custom-name-no-property-name/expected-remove-es6.js
@@ -2,11 +2,6 @@ var {
   createClass
 } = require('./notReact');
 
-var PropTypes = require('prop-types');
-
 var Class1 = createClass({
-  displayName: 'Class1',
-  propTypes: {
-    foo: PropTypes.string
-  }
+  displayName: 'Class1'
 });
